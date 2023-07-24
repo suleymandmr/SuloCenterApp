e0944d549786fd799355f75e724aed2f5817d0cc
Süleyman Demir <suleymandemir75@hotmail.com>
2023-07-24 12:40:10 +0300

    Initial Commit

---
+++ SuloCenterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuloCenterApp
+//
+//  Created by eyüp yaşar demir on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

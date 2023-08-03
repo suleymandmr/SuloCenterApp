a8b0c79ce2f89836dab73463ca4ea682f883aba5
Süleyman Demir <suleymandemir75@hotmail.com>
2023-08-03 11:59:23 +0300

    picker view oluşturma

--- SuloCenterApp/Magazalar/MagazalarViewController.swift
+++ SuloCenterApp/Magazalar/MagazalarViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 import Firebase
 import SDWebImage
 
-class MagazalarViewController: UIViewController {
-
+class MagazalarViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSource{
+    let kattar = ["Zemin Kat", "1. Kat", "2. Kat", "3. Kat", "4. Kat", "5. Kat", "6. Kat", "7. Kat", "8. Kat"]
+    var pickerView: UIPickerView!
     var imageArray = [String]()
     var nameArray = [String]()
     var numberArray = [String]()
+    var selectedKat: String?
+    
     
     let fireStoreDatabase = Firestore.firestore()
     @IBOutlet weak var tableView: UITableView!
@@ -24,9 +27,54 @@ class MagazalarViewController: UIViewController {
     
         getDataFromFirebase()
         
+        
+        pickerView = UIPickerView()
+                pickerView.delegate = self
+                pickerView.dataSource = self
+                view.addSubview(pickerView)
+                pickerView.isHidden = true // Başlangıçta gizli olacak
+
+        // UIPickerView nesnesini oluşturun ve ekranınıza eklemeyin (ilk başta gizli olacak)
+        pickerView = UIPickerView()
+                pickerView.delegate = self
+                pickerView.dataSource = self
+                view.addSubview(pickerView)
+                pickerView.isHidden = true // Başlangıçta gizli olacak
+
+                // Katları seçmek için kullanılacak UIBarButtonItem'i oluşturun
+                let katBarButtonItem = UIBarButtonItem(title: "Kat Seç", style: .plain, target: self, action: #selector(showPickerView))
+                navigationItem.rightBarButtonItem = katBarButtonItem
        
     }
+    @objc func showPickerView() {
+         // UIPickerView'ı ekranın altına yerleştirin
+         let pickerHeight: CGFloat = 300 // İstediğiniz yüksekliği burada belirleyebilirsiniz
+         pickerView.frame = CGRect(x: 0, y: view.bounds.height - pickerHeight, width: view.bounds.width, height: pickerHeight)
+         pickerView.isHidden = false
+     }
 
+     // UIPickerViewDelegate fonksiyonları
+     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+         return kattar[row]
+     }
+
+     // UIPickerViewDataSource fonksiyonları
+     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+         return 1 // Tek bir sütun kullanıyoruz
+     }
+
+     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+         return kattar.count
+     }
+
+     // Kullanıcı bir kattı seçtiğinde yapılacak işlemler
+     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+         selectedKat = kattar[row]
+         // Seçilen katı kullanarak ilgili işlemleri yapabilirsiniz (örneğin, seçilen kattaki mağazaları listeleyebilirsiniz)
+         print("Seçilen kat: \(selectedKat ?? "")")
+         pickerView.isHidden = true // Kullanıcı bir kattı seçtikten sonra UIPickerView'ı gizleyin
+     }
+ 
     func getDataFromFirebase (){
         let fireStoreDatabase = Firestore.firestore()
         
@@ -66,6 +114,8 @@ class MagazalarViewController: UIViewController {
         }
     }
     
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
 }
 extension MagazalarViewController: UITableViewDelegate, UITableViewDataSource {
     
@@ -93,4 +143,3 @@ extension MagazalarViewController: UITableViewDelegate, UITableViewDataSource {
     }
    
 }
-
